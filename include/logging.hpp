6426b4157aa98341b6d14669b0e6481dd438e991
Hackysack <h4cky54ck@gmail.com>
2024-09-28 02:21:06 -0400

    Tidied up logging code.

--- include/logging.hpp
+++ include/logging.hpp
@@ -12,72 +12,42 @@ namespace logging {
   // Logger for more personalized logging
   struct Logger {
     std::string name;
-    std::size_t loglevel = 30;
+    std::size_t loglevel;
+    std::map<std::size_t, std::string> level_names;
+    std::map<std::size_t, std::string> level_colors;
 
-    bool use_color;
-    std::map<std::size_t, std::string> level_names = {
-      {10, "debug"},
-      {20, "info"},
-      {30, "warn"},
-      {40, "error"},
-      {50, "critical"},
-    };
-    std::map<std::size_t, std::string> level_colors = {
-      {0, "\x1b[00m"},
-      {10, "\x1b[36m"},
-      {20, "\x1b[32m"},
-      {30, "\x1b[33m"},
-      {40, "\x1b[31m"},
-      {50, "\x1b[41m"},
-    };
+    explicit Logger(std::string name, std::size_t loglevel);
+    explicit Logger(std::string name);
 
-
-    explicit Logger();
-    explicit Logger(std::string value1);
-    explicit Logger(std::string value1, std::size_t value2);
-
-    // Get loglevel (public)
-    void set_level(std::size_t level);
+    // Get and set level for an instance of a logger
     auto get_level() -> std::size_t;
+    void set_level(std::size_t level);
 
-    // Get name (public)
-    auto get_name() -> std::string;
-    void set_name(const std::string& value);
+    // Get level color for an instance of a logger
+    auto get_level_color(std::size_t level) -> std::string;
 
-    auto get_color(std::size_t level) -> std::string;
+    // Get level name for an instance of a logger
     auto get_level_name(std::size_t level) -> std::string;
 
     // Convenience methods
-    void log(std::size_t level, const std::string& msg, const std::string& tag);
     void log(std::size_t level, const std::string& msg);
-    void fatal(const std::string& msg, const std::string& tag);
     void fatal(const std::string& msg);
-    void critical(const std::string& msg, const std::string& tag);
     void critical(const std::string& msg);
-    void error(const std::string& msg, const std::string& tag);
     void error(const std::string& msg);
-    void warning(const std::string& msg, const std::string& tag);
     void warning(const std::string& msg);
-    void warn(const std::string& msg, const std::string& tag);
     void warn(const std::string& msg);
-    void info(const std::string& msg, const std::string& tag);
     void info(const std::string& msg);
-    void debug(const std::string& msg, const std::string& tag);
     void debug(const std::string& msg);
   };
 
   // Global convenience functions
   extern std::map<std::string, Logger> _logger_registry;
-  extern Logger& _logger;
 
   Logger& get_logger(const std::string& name);
 
-  void set_level(std::size_t level);
+  // Get and set level for global logger
   std::size_t get_level();
-
-  // Only helpful for global logger
-  void set_name(const std::string&);
-  std::string get_name();
+  void set_level(std::size_t level);
 
   // Convenience methods
   void log(std::size_t level, const std::string& msg);
--- src/logging.cpp
+++ src/logging.cpp
@@ -1,94 +1,60 @@
 #include "logging.hpp"
 
 
-// Local logger
-logging::Logger::Logger() : Logger("root") {}
-logging::Logger::Logger(std::string value1) : Logger(std::move(value1), 30) {}
-logging::Logger::Logger(std::string value1, std::size_t value2) : name(std::move(value1)), loglevel(value2), use_color(isatty(2) == 0) {}
+// Namespace structs and classes
 
-// Get loglevel (public)
-void logging::Logger::set_level(std::size_t level) {
-  loglevel = level;
+// Logger contstructors
+logging::Logger::Logger(std::string name, std::size_t loglevel) : name(std::move(name)), loglevel(loglevel) {
+  level_names = {{10, "debug"},{20, "info"},{30, "warn"},{40, "error"},{50, "critical"}};
+  level_colors = {{0, "\x1b[00m"},{10, "\x1b[36m"},{20, "\x1b[32m"},{30, "\x1b[33m"},{40, "\x1b[31m"},{50, "\x1b[41m"}};
 }
+logging::Logger::Logger(std::string name) : Logger(std::move(name), 30) {}
 
+// Get level for an instance of a logger
 auto logging::Logger::get_level() -> std::size_t {
   return loglevel;
 }
 
-// Get name (public)
-auto logging::Logger::get_name() -> std::string {
-  return name;
+// Set level for an instance of a logger
+void logging::Logger::set_level(std::size_t level) {
+  loglevel = level;
 }
 
-void logging::Logger::set_name(const std::string& value) {
-  name = value;
-}
-
-auto logging::Logger::get_color(std::size_t level) -> std::string {
-  if (use_color) {
-    return "";
-  }
+// Get level color for an instance of a logger
+auto logging::Logger::get_level_color(std::size_t level) -> std::string {
   return level_colors[level];
 }
 
+// Get level name for an instance of a logger
 auto logging::Logger::get_level_name(std::size_t level) -> std::string {
   return level_names[level];
 }
 
-
-
 // Emit local logger messages
-void logging::Logger::log(std::size_t level, const std::string& msg, const std::string& name) {
-  std::string tag = name;
-  if (name == "") {
-    tag = this->name;
-  }
-  if (loglevel <= level) {
-    std::clog << get_color(level) << "[" << tag << " " << get_level_name(level) << "]" << get_color(0) << ": " << msg << "\n";
-    std::clog.flush();
-  }
-}
 void logging::Logger::log(std::size_t level, const std::string& msg) {
-  log(level, msg, "");
-}
-void logging::Logger::fatal(const std::string& msg, const std::string& name) {
-  log(50, msg, name);
+  if (level >= loglevel) {
+    std::cerr << get_level_color(level) << "[" << name << " "
+              << get_level_name(level) << "]" << get_level_color(0)
+              << ": " << msg << "\n";
+  }
 }
 void logging::Logger::fatal(const std::string& msg) {
   log(50, msg);
 }
-void logging::Logger::critical(const std::string& msg, const std::string& name) {
-  log(50, msg, name);
-}
 void logging::Logger::critical(const std::string& msg) {
   log(50, msg);
-}
-void logging::Logger::error(const std::string& msg, const std::string& name) {
-  log(40, msg, name);
 }
 void logging::Logger::error(const std::string& msg) {
   log(40, msg);
 }
-void logging::Logger::warning(const std::string& msg, const std::string& name) {
-  log(30, msg, name);
-}
 void logging::Logger::warning(const std::string& msg) {
   log(30, msg);
-}
-void logging::Logger::warn(const std::string& msg, const std::string& name) {
-  log(30, msg, name);
 }
 void logging::Logger::warn(const std::string& msg) {
   log(30, msg);
 }
-void logging::Logger::info(const std::string& msg, const std::string& name) {
-  log(20, msg, name);
-}
 void logging::Logger::info(const std::string& msg) {
   log(20, msg);
-}
-void logging::Logger::debug(const std::string& msg, const std::string& name) {
-  log(10, msg, name);
 }
 void logging::Logger::debug(const std::string& msg) {
   log(10, msg);
@@ -96,6 +62,7 @@ void logging::Logger::debug(const std::string& msg) {
 
 
 
+// Namespace functions and members
 
 std::map<std::string, logging::Logger> logging::_logger_registry;
 
@@ -104,44 +71,40 @@ auto logging::get_logger(const std::string& name) -> Logger& {
   return _logger_registry.try_emplace(name, name).first->second;
 }
 
-// The "global" logger. It always exists.
-logging::Logger& logging::_logger = logging::get_logger("root");
-
-void logging::set_level(std::size_t level) {
-  _logger.loglevel = level;
-}
+// Get level for global logger
 auto logging::get_level() -> std::size_t {
-  return _logger.loglevel;
+  return get_logger("root").loglevel;
 }
 
-// Get and set name
-void logging::set_name(const std::string& name) {
-  _logger.name = name;
-}
-auto logging::get_name() -> std::string {
-  return _logger.name;
+// Set level for global logger
+void logging::set_level(std::size_t level) {
+  get_logger("root").loglevel = level;
 }
 
+// Convenience functions for the namespace
+void logging::log(std::size_t level, const std::string& msg) {
+  get_logger("root").log(level, msg);
+}
 void logging::fatal(const std::string& msg) {
-  _logger.log(50, msg);
+  log(50, msg);
 }
 void logging::critical(const std::string& msg) {
-  _logger.log(50, msg);
+  log(50, msg);
 }
 void logging::error(const std::string& msg) {
-  _logger.log(40, msg);
+  log(40, msg);
 }
 void logging::warning(const std::string& msg) {
-  _logger.log(30, msg);
+  log(30, msg);
 }
 void logging::warn(const std::string& msg) {
-  _logger.log(30, msg);
+  log(30, msg);
 }
 void logging::info(const std::string& msg) {
-  _logger.log(20, msg);
+  log(20, msg);
 }
 void logging::debug(const std::string& msg) {
-  _logger.log(10, msg);
+  log(10, msg);
 }
 
 
